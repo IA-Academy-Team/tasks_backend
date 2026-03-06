4fdf513679cd58aaf9d1b66800c8be91a98d3364
servorx <191423123+servorx@users.noreply.github.com>
2026-03-05 23:45:41 -0500

    feat: :coffin: remove logger middleware

--- src/app/create-app.ts
+++ src/app/create-app.ts
@@ -4,7 +4,6 @@ import { apiV1Router } from "./routes/v1.router.js";
 import { corsMiddleware } from "./middlewares/cors.middleware.js";
 import { errorHandlerMiddleware } from "./middlewares/error-handler.middleware.js";
 import { notFoundMiddleware } from "./middlewares/not-found.middleware.js";
-import { requestLoggerMiddleware } from "./middlewares/request-logger.middleware.js";
 
 export const createApp = () => {
   const app = express();
@@ -12,7 +11,6 @@ export const createApp = () => {
   app.use(morgan('dev'))
   app.disable("x-powered-by");
 
-  app.use(requestLoggerMiddleware);
   app.use(corsMiddleware);
   app.use(express.json({ limit: "1mb" }));
   app.use(express.urlencoded({ extended: true }));
